388b6a30e681412d6a7d79f0c36657c98d6a6eb0
Ruud0511 <post@ruud.co>
2023-06-24 16:25:38 +0200

    project afgerond en nagekeken

--- loops.js
+++ loops.js
@@ -104,6 +104,10 @@ for (let i = 2; i <= 10; i++) {
 
 
 for (let i = 0; i <= 15; i++) {
-    console.log(i);
+    if (i % 2 === 0) {
+        console.log(i + ' is even');
+    } else  {
+        console.log(i + ' is oneven')
+    }
 }
 
